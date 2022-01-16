e0cab8b7833851b3fe7bf1db2eae0e23b035e51e
Sanket khadse <55561403+edusanketdk@users.noreply.github.com>
2022-01-16 08:22:31 +0000

    fixed email

--- api/email_login.py
+++ api/email_login.py
@@ -5,7 +5,7 @@ from decouple import config
 def email_login():
     email, password = config("EMAILID"), config("PASSWORD")
     
-    server = smtplib.SMTP('smtp.gmail.com:587')
+    server = smtplib.SMTP('smtp.autocertify.tech:587')
     server.starttls()
     server.login(email, password)
     
