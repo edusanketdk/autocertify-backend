aa51fc53fe36f93deaa1d14dddffacbf624a278a
Sanket khadse <55561403+edusanketdk@users.noreply.github.com>
2022-01-17 05:11:35 +0000

    replace email

--- api/email_login.py
+++ api/email_login.py
@@ -5,7 +5,8 @@ from decouple import config
 def email_login():
     email, password = config("EMAILID"), config("PASSWORD")
     
-    server = smtplib.SMTP('us2.smtp.mailhostbox.com:587')
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    #server = smtplib.SMTP('us2.smtp.mailhostbox.com:587')
     server.starttls()
     server.login(email, password)
     
